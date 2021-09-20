ff59d310f267a8e74e67374f975d916f2d0be8e7
Iliana00 <carrasquedo.279@gmail.com>
2021-09-20 12:05:46 -0500

    test login

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import { activeSession } from './lib/firebase.js';
 
 const component = routes[window.location.pathname];
 
-document.getElementById('root').appendChild(component());
+document.getElementById('root').appendChild(component);
 
 activeSession();
