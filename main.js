5657a0897f5af50999471b868b06088dd6be275e
Iliana00 <carrasquedo.279@gmail.com>
2021-09-20 12:09:41 -0500

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-import { routes } from './router/router.js';
+// import { routes } from './router/router.js';
+// import { activeSession } from './lib/firebase.js';
+
+// const component = routes[window.location.pathname];
+
+// document.getElementById('root').appendChild(component());
+
+// activeSession();
 import { activeSession } from './lib/firebase.js';
-
-const component = routes[window.location.pathname];
-
-document.getElementById('root').appendChild(component);
 
 activeSession();
